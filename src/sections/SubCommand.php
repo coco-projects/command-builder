588fa86cdf756a3d990111f8105137e9be0688e0
coco-projects <coco-project@protonmail.com>
2024-11-24 12:10:44 +0800

    update

---
+++ src/sections/SubCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+    declare(strict_types = 1);
+
+    namespace Coco\commandBuilder\sections;
+
+    use Coco\commandBuilder\abstract\SectionAbstract;
+    use Coco\commandBuilder\Builder;
+
+    // list
+class SubCommand extends SectionAbstract
+{
+    protected string|int|Builder $value = '';
+
+    public function __construct(string|int|Builder $value)
+    {
+        $this->value = $value;
+    }
+
+    public function toString(): string
+    {
+        return trim((string)$this->value);
+    }
+}
